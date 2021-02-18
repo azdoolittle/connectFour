214d59913a630576fc172f50ab9ec7da26506faf
Andrew Doolittle <drewdoolittle@gmail.com>
2021-02-17 22:52:06 -0500

    Basic component logic

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from './Board';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div className="board">
+        <Board>Board</Board>
+      </div>
+    )
+  }
+} 
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import Main from "./components/Main";
+
+ReactDOM.render(<Main />, document.getElementById('main'));
